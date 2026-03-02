7809860b75f4c21e8f940219e74a76725f0c0790
cambrianaman <akaurca09@gmail.com>
2026-03-02 16:10:42 -0500

    Intial Commit

---
+++ demo.js
@@ -0,0 +1,81 @@
+// dynamic  Input
+// Import the readline module
+
+const readline = require('readline'); 
+
+// Create an interface for input/output
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+function h()
+{
+    console.log(" *****************  Calculator *************");
+}
+h();
+ 
+function Calculator(numa,numb)
+{
+    console.log("Press 1 for addition");
+    console.log("Press 2 for subtraction");
+    console.log("Press 3 for multiplication");
+    console.log("Press 4 for division");
+    console.log("Press 5 for modulous");
+    rl.question("Enter first number: ", (firstInput) => {
+    var numa = parseFloat(firstInput);
+    if (isNaN(numa)) {
+        console.error("Invalid number.");
+        rl.close();
+        return;
+    }
+
+ rl.question("Enter second number: ", (secondInput) => {
+            var numb = parseFloat(secondInput);
+            if (isNaN(numb)) {
+                console.error("Invalid number.");
+                rl.close();
+                return;
+            } 
+        
+       
+         rl.question("Enter choice: ", (choice) => {
+            var op = parseFloat(choice);
+            if (isNaN(numb)) {
+                console.error("Invalid number.");
+                rl.close();
+                return;
+            }
+    
+
+    switch (op)
+{
+        case 1: 
+        console.log(numa+numb);
+        case 2: 
+         console.log(numa+numb);
+        case 3: 
+         console.log(numa+numb);
+        case 4: 
+         console.log(numa+numb);
+        case 5: 
+         console.log(numa+numb);
+//var c=a+b;
+//var d=a-b;
+//var e=a*b;
+//var f=a/b;
+//var g=a % b;
+//console.log("Addition of two numbers = "+c);
+//console.log("Subtraction of two numbers = "+d);
+//console.log("Multiplication of two numbers = "+e);
+//console.log("Division of two numbers = "+f);
+//console.log("Modulous of two numbers = "+g);
+}
+}
+
+
+
+});
+});
+});
+Calculator(numa, numb);
